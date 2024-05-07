120061d2170d420969be8e4a9608fe0094d5ee85
mehediccb <mehedi.hassan@nusaiba.com.bd>
2024-05-07 11:56:56 +0600

    change mongodb

--- product-service/server.js
+++ product-service/server.js
@@ -15,7 +15,8 @@ startAllUsersConsumer().catch(console.error);
 app.use("/products", productRoutes);
 
 mongoose
-  .connect("mongodb://mongo-products:27017/products") //for docker
+  // .connect("mongodb://mongo-products:27017/products") //for docker
+  .connect("mongodb://mongo:27017/products") //for docker
   // .connect("mongodb://localhost:27017/products")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB:", err));
