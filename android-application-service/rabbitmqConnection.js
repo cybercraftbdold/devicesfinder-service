97d6fbd85d0e5545b6dc2e7f58f7bd38d795db19
mehediccb <mehedi.hassan@nusaiba.com.bd>
2024-05-07 13:50:51 +0600

    added my rabbitmq

--- android-application-service/rabbitmqConnection.js
+++ android-application-service/rabbitmqConnection.js
@@ -1,18 +1,19 @@
-const amqp = require('amqplib');
+const amqp = require("amqplib");
 
 // Use the CloudAMQP URL from your credentials
-const RABBITMQ_URL = 'amqps://zyhofqvj:cUzKnldbzpfhkWMvlK4-tIKUzJNG4kdE@kangaroo.rmq.cloudamqp.com/zyhofqvj';
+// const RABBITMQ_URL = 'amqps://zyhofqvj:cUzKnldbzpfhkWMvlK4-tIKUzJNG4kdE@kangaroo.rmq.cloudamqp.com/zyhofqvj';
+const RABBITMQ_URL = "amqp://cybercraft:cybercraft@195.35.32.241:5672/";
 
 async function connectRabbitMQ() {
-    try {
-        const connection = await amqp.connect(RABBITMQ_URL);
-        const channel = await connection.createChannel();
-        await channel.assertQueue('userDataQueue');  // Queue for user creation events
-        return channel;
-    } catch (error) {
-        console.error('Failed to connect to RabbitMQ:', error);
-        throw error;
-    }
+  try {
+    const connection = await amqp.connect(RABBITMQ_URL);
+    const channel = await connection.createChannel();
+    await channel.assertQueue("userDataQueue"); // Queue for user creation events
+    return channel;
+  } catch (error) {
+    console.error("Failed to connect to RabbitMQ:", error);
+    throw error;
+  }
 }
 
 module.exports = connectRabbitMQ;
