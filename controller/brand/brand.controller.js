5383b5c78ec5cba1023e6b71b036838b762e3e29
shoaibccb <shoaib.akhter@nusaiba.com.bd>
2024-06-05 14:43:15 +0600

    get all brands api is done

--- controller/brand/brand.controller.js
+++ controller/brand/brand.controller.js
@@ -1,6 +1,6 @@
 const {
   createBrandService,
-  getBrandService,
+  getAllBrandService,
 } = require("../../services/brand/brand.service");
 
 // Create Brand
@@ -10,13 +10,13 @@ const createBrandController = async (req, res) => {
     const result = await createBrandService(payload);
 
     if (result.isSuccess) {
-      res.json({
+      res.status(201).json({
         message: result.message,
         isSuccess: result.isSuccess,
         data: result?.response,
       });
     } else {
-      res.json({
+      res.status(500).json({
         message: result.message,
         isSuccess: false,
       });
@@ -27,6 +27,43 @@ const createBrandController = async (req, res) => {
 };
 
 // Get All Brands
-const getAllBrandsController = async (req, res) => {};
+const getAllBrandsController = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    const searchText = req?.query?.searchText;
+    const sortField = req?.query?.sortField || "createdAt";
+    const sortOrder = req?.query?.sortOrder || "desc";
+
+    const result = await getAllBrandService(
+      limit,
+      skip,
+      searchText,
+      sortField,
+      sortOrder
+    );
+
+    if (
+      result &&
+      result.isSuccess &&
+      result.response &&
+      result.response.length > 0
+    ) {
+      res.status(200).json({
+        message: result?.message,
+        isSuccess: result.isSuccess,
+        totalItems: result?.response[0]?.totalCount[0]?.value || 0,
+        totalLength: result?.response[0].data?.length,
+        data: result?.response[0].data,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      isSuccess: false,
+    });
+  }
+};
 
 module.exports = { createBrandController, getAllBrandsController };
--- services/brand/brand.service.js
+++ services/brand/brand.service.js
@@ -1,6 +1,6 @@
 const BrandModel = require("../../models/brand-model/brand-model");
 
-// create brand
+// Create Brand
 const createBrandService = async (payload) => {
   let { title, image } = payload;
 
@@ -28,7 +28,49 @@ const createBrandService = async (payload) => {
   }
 };
 
-// get brands
-const getBrandService = async () => {};
+// Get All Brands
+const getAllBrandService = async (
+  limit,
+  skip,
+  searchText,
+  sortField = "createdAt",
+  sortOrder = "desc"
+) => {
+  try {
+    let query = {};
+    if (searchText) {
+      query.$or = [{ title: { $regex: searchText, $options: "i" } }];
+    }
 
-module.exports = { createBrandService, getBrandService };
+    // Determine sort order
+    const sort = {};
+    sort[sortField] = sortOrder.toLowerCase() === "asc" ? 1 : -1;
+
+    // Getting brands from database
+    const brands = await BrandModel.aggregate([
+      { $match: query },
+      { $sort: sort },
+      {
+        $facet: {
+          data: [{ $skip: skip }, { $limit: limit }],
+          totalCount: [{ $count: "value" }],
+        },
+      },
+    ]);
+
+    if (brands) {
+      return {
+        isSuccess: true,
+        response: brands,
+        message: "Data getting successfully",
+      };
+    }
+  } catch (error) {
+    return {
+      isSuccess: false,
+      message: error.message,
+    };
+  }
+};
+
+module.exports = { createBrandService, getAllBrandService };
