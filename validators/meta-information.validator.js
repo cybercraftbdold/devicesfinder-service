f1e65546e13a9e73a6972fd4630d5c758ab10828
shoaibccb <shoaib.akhter@nusaiba.com.bd>
2024-06-12 17:30:49 +0600

    validation schema for meta information is modified for canonical url

--- validators/meta-information.validator.js
+++ validators/meta-information.validator.js
@@ -2,7 +2,10 @@ const Joi = require("joi");
 
 // Define the Joi schema for metaInformation
 const metaInformationSchemaValidator = Joi.object({
-  canonicalUrl: Joi.string().required(),
+  canonicalUrl: Joi.string()
+    .pattern(/^[a-zA-Z0-9-]+$/)
+    .message("Canonical URL must only contain letters, numbers, and hyphens")
+    .required(),
   mainKeyword: Joi.string().required(),
   metaDescription: Joi.string().required(),
   metaTitle: Joi.string().required(),
