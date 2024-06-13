567e5a8a10ff3f8b9d7852ba62ea521d3a094bfc
shoaibccb <shoaib.akhter@nusaiba.com.bd>
2024-06-13 11:37:49 +0600

    faq schema is modified

--- services/faq/faq.service.js
+++ services/faq/faq.service.js
@@ -2,15 +2,22 @@ const FaqModel = require("../../models/faq-model/faq.model");
 
 // Create Faq
 const createFaqService = async (payload) => {
-  let { title, deviceId, description, metaInformation } = payload;
+  let { title, deviceId, faqList } = payload;
 
   try {
+    const duplicateFaq = await FaqModel.findOne({ deviceId });
+
+    if (duplicateFaq)
+      return {
+        isSuccess: false,
+        message: "Faq for this device is already created",
+      };
+
     // Proceed to create a new DeviceReviewModel instance with the provided payload
     const faq = new FaqModel({
       title,
       deviceId,
-      description,
-      metaInformation,
+      faqList,
     });
 
     // Attempt to save the new blog post to the database
