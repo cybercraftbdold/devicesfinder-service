18e6622786d611ae8ab705cdcd03d4ed357940bc
mehediccb <mehedi.hassan@nusaiba.com.bd>
2024-06-21 12:04:30 +0600

    base website

--- validators/brand.validator.js
+++ validators/brand.validator.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 const brandSchemaValidator = Joi.object({
   title: Joi.string().required(),
   image: Joi.string().required(),
+  websiteBase: Joi.string().optional(),
 });
 
 module.exports = brandSchemaValidator;
