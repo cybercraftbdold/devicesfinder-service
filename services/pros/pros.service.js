86c4f9be99f60a7380da5ed18405f5334442aa53
shoaibccb <shoaib.akhter@nusaiba.com.bd>
2024-06-25 12:58:23 +0600

    pros api modified

--- services/pros/pros.service.js
+++ services/pros/pros.service.js
@@ -1,3 +1,4 @@
+const updateWithDeviceIdService = require("../../helpers/service-helpers/updateWithDeviceId");
 const ProsModel = require("../../models/pros-model/pros.model");
 
 // Create Device Review
@@ -9,12 +10,10 @@ const createProsService = async (payload) => {
       deviceId,
     });
 
-    // Checking for duplicate brand
+    // duplicate pros are updating
     if (duplicatePros)
-      return {
-        isSuccess: false,
-        message: "Already have a pros with same deviceId.",
-      };
+      return await updateWithDeviceIdService(payload, ProsModel, "Pros");
+
     // Proceed to create a new ProsModel instance with the provided payload
     const pros = new ProsModel({
       title,
@@ -29,7 +28,7 @@ const createProsService = async (payload) => {
       return {
         isSuccess: true,
         response: newPros,
-        message: "Device Pros created successfully",
+        message: "Device Pros published successfully",
       };
     }
   } catch (error) {
@@ -40,6 +39,65 @@ const createProsService = async (payload) => {
   }
 };
 
+// Get All Pros
+const getAllProsService = async (
+  limit,
+  skip,
+  searchText,
+  filters,
+  sortField = "createdAt",
+  sortOrder = "desc"
+) => {
+  try {
+    let query = {};
+
+    if (searchText) {
+      query.$or = [{ title: { $regex: searchText, $options: "i" } }];
+    }
+
+    // Apply filters if they are provided
+    if (filters) {
+      if (filters.deviceId) {
+        query.deviceId = filters.deviceId;
+      }
+    }
+
+    // Determine sort order
+    const sort = {};
+    sort[sortField] = sortOrder.toLowerCase() === "asc" ? 1 : -1;
+
+    const res = await ProsModel.aggregate([
+      { $match: query },
+      { $sort: sort },
+      {
+        $facet: {
+          data: [{ $skip: skip }, { $limit: limit }],
+          totalCount: [{ $count: "value" }],
+        },
+      },
+    ]);
+    if (res) {
+      return {
+        isSuccess: true,
+        response: res[0],
+        message: "Data getting successful",
+      };
+    } else {
+      return {
+        isSuccess: true,
+        response: [],
+        message: "No data found",
+      };
+    }
+  } catch (error) {
+    return {
+      isSuccess: false,
+      message: error.message,
+    };
+  }
+};
+
 module.exports = {
   createProsService,
+  getAllProsService,
 };
--- validators/pros.validator.js
+++ validators/pros.validator.js
@@ -1,11 +1,14 @@
 const Joi = require("joi");
+const metaInformationSchemaValidator = require("./meta-information.validator");
 
 // Define the Joi schema for the pros
 const prosSchemaValidator = Joi.object({
   title: Joi.string().required(),
   deviceId: Joi.string().optional(),
+  reviewStatus: Joi.string().required(),
   websiteBase: Joi.string().optional(),
   description: Joi.string().required(),
+  metaInformation: metaInformationSchemaValidator,
 });
 
 module.exports = prosSchemaValidator;
