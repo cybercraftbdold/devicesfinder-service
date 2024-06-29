4a8065ef7bca8ca0fdf03ec2021cffd30f36101c
shoaibccb <shoaib.akhter@nusaiba.com.bd>
2024-06-29 18:21:43 +0600

    reviewStatus added to blog service

--- services/blog/blog.service.js
+++ services/blog/blog.service.js
@@ -15,6 +15,7 @@ const createBolgService = async (payload) => {
     categories,
     image,
     metaInformation,
+    reviewStatus,
   } = payload;
 
   try {
@@ -35,6 +36,7 @@ const createBolgService = async (payload) => {
       image,
       categories,
       metaInformation,
+      reviewStatus,
     });
 
     // Attempt to save the new blog post to the database
