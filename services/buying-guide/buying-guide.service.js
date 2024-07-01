458e290da2e53dad8b4784a8e3e5e858a6bde682
shoaibccb <shoaib.akhter@nusaiba.com.bd>
2024-07-01 17:01:57 +0600

    comparison delete api created

--- services/buying-guide/buying-guide.service.js
+++ services/buying-guide/buying-guide.service.js
@@ -102,6 +102,7 @@ const getAllBuyingGuideService = async (
   }
 };
 
+// delete Buying guide
 const deleteBuyingGuideService = async (id) => {
   return await deleteItem(id, BuyingGuideModel);
 };
